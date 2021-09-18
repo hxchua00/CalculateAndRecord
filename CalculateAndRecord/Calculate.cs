96e393a375a7864ac25b230a9cd00490da164d20
hxchua00 <hxchua00@gmail.com>
2021-09-18 20:54:33 +0800

    Add project files.

---
+++ CalculateAndRecord/Calculate.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateAndRecord
+{
+    class Calculate
+    {
+        //Global Variables
+        int x, y;
+        //int addCount, multCount, minusCount, divideCount; //Counters
+        static int count;
+        int input1, input2;
+        
+        static Calculate()
+        {
+            count = 0;
+        }
+
+        public Calculate() { }
+
+        public Calculate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void InputMsg()
+        {
+            Calculate c = new Calculate(input1, input2);          
+
+            Console.WriteLine("Enter first number: ");
+            input1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second number: ");
+            input2 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+        }
+
+        public void Add()
+        {
+            InputMsg();
+            Console.WriteLine("Adding...");
+            Console.WriteLine("Result is " + (input1 + input2));
+            Console.WriteLine();
+            count++;
+        }
+        public void Multuply()
+        {
+            InputMsg();
+            Console.WriteLine("Multiplying...");
+            Console.WriteLine("Result is " + (input1 + input2));
+            Console.WriteLine();
+            count++;
+        }
+        public void Minus()
+        {
+            InputMsg();
+            Console.WriteLine("Subtracting...");
+            if (input1 > input2)
+            {
+                Console.WriteLine("Result is " + (input1 - input2));
+            }
+            else
+            {
+                Console.WriteLine("Result is " + (input2 - input1));
+            }
+
+            Console.WriteLine();
+            count++;
+        }
+        public void Divide()
+        {
+            InputMsg();
+            Console.WriteLine("Adding...");
+            if (input1 != 0 && input2 != 0)
+            {
+                if (input1 > input2)
+                {
+                    Console.WriteLine("Result of " + input1 + " divided by " + input2 + " is " + (input1 / input2));
+                }
+                else
+                {
+                    Console.WriteLine("Result of " + input2 + " divided by " + input1 + " is " + (input2 / input1));
+                }
+            }
+
+            Console.WriteLine();
+            count++;
+        }
+
+        public void showUsedOperators()
+        {
+            //Console.WriteLine("Number of additions: " + addCount);
+            //Console.WriteLine("Number of subtractions: " + minusCount);
+            //Console.WriteLine("Number of multiplications: " + multCount);
+            //Console.WriteLine("Number of divisions: " + divideCount);
+            //Console.WriteLine();
+            //Console.WriteLine("Total number of operators used is " + (addCount + minusCount + multCount + divideCount));
+            //Console.WriteLine();
+            Console.WriteLine("Total number of operations: " + count);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ CalculateAndRecord/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateAndRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start calculating!");
+            Console.WriteLine("");
+
+            Calculate c = new Calculate();
+            bool condition = true;
+            while (condition)
+            {
+                Console.WriteLine("1) Add");
+                Console.WriteLine("2) Subtract");
+                Console.WriteLine("3) Multiply");
+                Console.WriteLine("4) Divide");
+                Console.WriteLine("5) Show all used operators");
+                Console.WriteLine("6) Exit");
+                Console.WriteLine();
+
+                Console.Write("Enter your option: ");
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:                        
+                        c.Add();
+                        break;
+                    case 2:
+                        c.Minus();
+                        break;
+                    case 3:
+                        c.Multuply();
+                        break;
+                    case 4:
+                        c.Divide();
+                        break;
+                    case 5:
+                        c.showUsedOperators();
+                        break;
+                    case 6:
+                        Console.WriteLine("Good bye!");
+                        condition = false;
+                        break;
+                    default:
+                        Console.WriteLine("Choose from the option!");
+                        break;
+                }
+            }
+
+            Console.ReadLine();
+           
+        }
+    }
+}
